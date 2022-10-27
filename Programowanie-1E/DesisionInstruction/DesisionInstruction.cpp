96888845118d2a2ef2d425a68f2cfbb31c8fda25
bobek <bobbudowniczy123@gmail.com>
2022-10-27 15:10:48 +0200

    d

--- Programowanie-1E/DesisionInstruction/DesisionInstruction.cpp
+++ Programowanie-1E/DesisionInstruction/DesisionInstruction.cpp
@@ -4,6 +4,18 @@
 DRY - Don't Repeat Yourself
 */
 
+
+/*
+	Operatory porównania:
+*równoœci ==
+* róznoœci !=
+* wiêkszoœci >
+* mniejszoœci <
+* wieksze lub róne >=
+* mniejsze lub równe <=
+*/
+
+
 /*
 Napisz funkcje ktora wczyta dwie liczby i wykona dzielenie na nich 
 */
@@ -32,7 +44,42 @@ void task1()
 		std::cout << "koniec programu";
 }
 
+/*
+Napisz funkcje ktora czyta licxzbe i wyswietli czy jest dodatia czy nie.
+*/
+
+void task2()
+{
+	int NumberToCheck;
+
+	std::cout << "Podaj liczbe do sprawdzenia:\n";
+	std::cin >> NumberToCheck;
+
+	if (NumberToCheck > 0)
+		std::cout << "liczba wieksza od zera\n";
+	else
+		std::cout << "liczba równa lub mniejsza od zera\n";
+}
+
+/*
+Napisz funkcje, która wczsyta liczbe i wyswietli informacje czy jest ona w zakresie <10, 25>
+*/
+
+void task3()
+{
+	int NumberToCheck2;
+
+	std::cout << "Podaj liczbe do sprawdzenia:\n";
+	std::cin >> NumberToCheck2;
+
+	if (NumberToCheck2 )
+		std::cout << "liczba znajduje sie w zakresie\n";
+	else
+		std::cout << "liczba nie znajduje sie w zakresie\n";
+}
 int main()
 {
-	task1();
+	//task1();
+	//task2();
+	task3();
 }
