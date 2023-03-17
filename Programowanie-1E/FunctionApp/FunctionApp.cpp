79ceb7cf1c3ccee2df42b7629fb6171c17172037
xamel <kamilborek9@wp.pl>
2023-03-17 15:13:25 +0100

    17.03.2023

---
+++ Programowanie-1E/FunctionApp/FunctionApp.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+void ShowTextForUser()
+{
+	std::cout << "Podaj pierwsz? liczb?:\n";
+}
+
+int GetFirstNumber()
+{
+	int x;
+	ShowTextForUser();
+	std::cin >> x;
+	return x;
+}
+
+void task1()
+{
+	int firstNumberFromUser = 2;
+
+	firstNumberFromUser = GetFirstNumber();
+
+	std::cout << "Podwojona warto?? to " << (firstNumberFromUser * 2) << "\n";
+}
+
+void task2()
+{
+	int firstNumberFromUser = 2;
+
+	firstNumberFromUser = GetFirstNumber();
+
+	if (firstNumberFromUser > 0)
+		std::cout << "Liczba dodatnia\n";
+	else
+		std::cout << "Liczba ujemna\n";
+}
+
+void task3()
+{
+	double y;
+
+	y = sqrt(8);
+
+	std::cout << y;
+}
+
+
+int main()
+{
+	//task3();
+	task1();
+	//task2();
+	//task1();
+
+}
