ba866d770e208f0b4c9d7ac42061c0df39011517
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-14 15:09:51 +0100

    14.12.2023

--- Programowanie-1E/LibraryConsoleAplication/LibraryConsoleAplication.cpp
+++ Programowanie-1E/LibraryConsoleAplication/LibraryConsoleAplication.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
+struct book
+{
+	std::string title;
+	std::string author;
+	short publicationYear;
+	unsigned short numberOfPages;
+};
+
+void menu()
+{
+	system("cls");
+	std::cout << "BIBLIOTEKA";
+	std::cout << "1. \n";
+
+	std::cout << "0. koniec programu\n";
+}
+
+int getOption()
+{
+	std::cout << "Wybierz opcje: ";
+	int option;
+	std::cin >> option;
+	return option;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int BOOK_SIZE = 100;
+	book colectionOfBooks[BOOK_SIZE];
+	int currentNumberOfBooks;
+
+	while (true)
+	{
+		menu();
+		int chosenOption = getOption();
+
+		if (chosenOption == 0)
+			break;
+	}
 }
