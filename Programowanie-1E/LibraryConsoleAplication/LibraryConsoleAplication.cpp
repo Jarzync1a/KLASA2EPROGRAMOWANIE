b54beb33925a85aa11547498168a317042a42f31
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-09 15:04:57 +0100

    09.01.2024

--- Programowanie-1E/LibraryConsoleAplication/LibraryConsoleAplication.cpp
+++ Programowanie-1E/LibraryConsoleAplication/LibraryConsoleAplication.cpp
@@ -8,11 +8,27 @@ struct book
 	unsigned short numberOfPages;
 };
 
+void showAllBooks(book books[], int currentNumberOfBooks)
+{
+	system("cls");
+	std::cout << "Wyswietlenie wszystkich pozycji: \n";
+	for (int i = 0; i <= currentNumberOfBooks; i++)
+	{
+		std::cout << "-----------------------------------------------\n";
+		std::cout << "Tytu³ " << books[i].title << "\n";
+		std::cout << "Autor " << books[i].author << "\n";
+		std::cout << "Rok wydania " << books[i].publicationYear << "\n";
+		std::cout << "Ilosc stron " << books[i].numberOfPages << "\n";
+	}
+	system("pause");
+}
+
 void menu()
 {
 	system("cls");
-	std::cout << "BIBLIOTEKA";
-	std::cout << "1. \n";
+	std::cout << "BIBLIOTEKA\n";
+	std::cout << "1. Dodanie nowej ksiazki \n";
+	std::cout << "2. Wyswietlenie wszystkich ksiazek \n";
 
 	std::cout << "0. koniec programu\n";
 }
@@ -25,11 +41,25 @@ int getOption()
 	return option;
 }
 
+void addNewBook(book books[], int &currentNumberOfBooks)
+{
+	currentNumberOfBooks++;
+	std::cout << "Podaj tytu³:\n";
+	std::cin >> books[currentNumberOfBooks].title;
+	std::cout << "Podaj autora:\n";
+	std::cin >> books[currentNumberOfBooks].author;
+	std::cout << "Podaj date wydania:\n";
+	std::cin >> books[currentNumberOfBooks].publicationYear;
+	std::cout << "Podaj ilosc stron:\n";
+	std::cin >> books[currentNumberOfBooks].numberOfPages;
+}
+
 int main()
 {
+	setlocale(LC_CTYPE, "polish");
 	const int BOOK_SIZE = 100;
 	book colectionOfBooks[BOOK_SIZE];
-	int currentNumberOfBooks;
+	int    currentNumberOfBooks = -1;
 
 	while (true)
 	{
@@ -38,5 +68,16 @@ int main()
 
 		if (chosenOption == 0)
 			break;
+
+		switch (chosenOption)
+		{
+		case 1:
+			addNewBook(colectionOfBooks, currentNumberOfBooks);
+			break;
+		
+		case 2:
+			showAllBooks(colectionOfBooks, currentNumberOfBooks);
+			break;
+		}
 	}
 }
