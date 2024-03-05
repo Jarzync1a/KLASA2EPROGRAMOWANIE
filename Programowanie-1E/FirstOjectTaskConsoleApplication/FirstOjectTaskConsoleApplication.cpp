156eb2b7bf093adc04832d4af8a8b97b7a6f9cdc
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-05 15:00:00 +0100

    05.03.2024

---
+++ Programowanie-1E/FirstOjectTaskConsoleApplication/FirstOjectTaskConsoleApplication.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+/*
+Zdefiniuj klase opisuj¹cy punkt na przestrzeni kartezjañskiej. Mo¿liwoœci klasy:
+* konstruktory
+* metoda która zwórci odleg³oœæ tego punktu od poczatku uk³adu wspó³rzednych
+* metoda która zwróci odleg³oœæ od innego punktu
+* metoda która zwróci informacjê w której æwiartce uk³adu wspó³rzednych siê znajduje punkt
+* metodê która poka¿e informacje o punkcie.
+
+Napisz program który zaprezentuje mo¿liwoœci obiektu na podstawie tej klasy.
+
+*/
+
+class Point2D
+{
+private:
+	double x;
+	double y;
+
+public:
+	Point2D()
+	{
+		x = 10;
+		y = 6;
+	}
+
+	Point2D(double xx)
+	{
+		x = xx;
+		y = 6;
+	}
+
+	Point2D(double xx, double yy)
+	{
+		x = xx;
+		y = yy;
+	}
+
+	double DistanceFromCenter(Point2D SecondPoint)
+	{
+		double distance = sqrt(x * x + y * y);
+		return distance;
+	}
+
+	double DistanceBetweenTwoPoints(Point2D secondpoint)
+	{
+		double distance = sqrt((x - secondpoint.x) * (x - secondpoint.x) + (y - secondpoint.y) * (y - secondpoint.y);
+		return distance;
+	}
+};
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+
+	Point2D firstPoint2D;
+	
+	std::cout << "Odleg³oœæ od œrodka wynosi: " << firstPoint2D.DistanceFromCenter() << "\n";
+
+	Point2D secondPoint2D(2, 8.5);
+
+	std::cout << "Odleg³oœc pomiedzy punktami: " << firstPoint2D.DistanceBetweenTwoPoints() << "\n";
+}
