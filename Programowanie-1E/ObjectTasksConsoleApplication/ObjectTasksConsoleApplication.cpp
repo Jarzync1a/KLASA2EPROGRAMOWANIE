c2ec218f1dc078c23d41ba8d5a3e5006dcf55cb0
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-05-16 15:12:05 +0200

    16.02.2024

--- Programowanie-1E/ObjectTasksConsoleApplication/ObjectTasksConsoleApplication.cpp
+++ Programowanie-1E/ObjectTasksConsoleApplication/ObjectTasksConsoleApplication.cpp
@@ -21,39 +21,39 @@ W Main utwórz kilka obiektów klasy Licz i wykonaj ró¿ne operacje.
 class Licz
 {
 private:
-    int number;
+	int number;
 public:
-    Licz()
-    {
-        number = 0;
-    }
+	Licz()
+	{
+		number = 0;
+	}
 
-    Licz(int a)
-    {
-        number = a;
-    }
+	Licz(int a)
+	{
+		number = a;
+	}
 
-    void increseNumber(int a)
-    {
-        number += a;
-    }
+	void increseNumber(int a)
+	{
+		number += a;
+	}
 
-    void decreseNumber(int a)
-    {
-        number -= a;
+	void decreseNumber(int a)
+	{
+		number -= a;
 
-    }
+	}
 
 };
 
 
 int main()
 {
-    Licz a;
-    Licz b(5);
+	Licz a;
+	Licz b(5);
 
-    a.decreseNumber(2);
-    b.increseNumber(1);
+	a.decreseNumber(2);
+	b.increseNumber(1);
 }
 */
 
@@ -73,81 +73,88 @@ Stwórz klasê Sumator z :
 
 • metod¹ wypisuj¹c¹ wszystkie elementy tablicy
 
-• metod¹ przyjmuj¹c¹ dwa parametry : lowIndex oraz highIndex, która wypisze elementy o indeksach >= lowIndex oraz <= highIndex.Metoda powinna zadzia³aæ 
+• metod¹ przyjmuj¹c¹ dwa parametry : lowIndex oraz highIndex, która wypisze elementy o indeksach >= lowIndex oraz <= highIndex.Metoda powinna zadzia³aæ
 poprawnie, gdy lowIndex lub highIndex wykraczaj¹ poza zakres tablicy(pomin¹æ te elementy).
 */
 
 class Sumator
 {
 private:
-    int table[10];
+	int table[10];
 
 public:
-    Sumator()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            table[i] = 0;
-        }
-    }
+	Sumator()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			table[i] = 0;
+		}
+	}
 
-    Sumator(int a)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            table[i] = rand() % 100;
-        }
-    }
+	Sumator(int a)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			table[i] = rand() % 10;
+		}
+	}
 
-    void sumOfNumbers()
-    {
-        int sum = 0;
-        for (int i = 0; i < 10; i++)
-        {
-            sum = table[i] + sum;
-        }
-        std::cout << "suma podanych liczb wynosi = " << sum << "\n";
-    }
+	void sumOfNumbers()
+	{
+		int sum = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			sum = table[i] + sum;
+		}
+		std::cout << "suma podanych liczb wynosi = " << sum << "\n";
+	}
 
-    void SumaPodziel3()
-    {
-        int sumOfNumbersDivisibleBy3 = 0;
-        for (int i = 0; i < 10; i++)
-        {
-            if (table[i] % 3 == 0)
-            {
-                sumOfNumbersDivisibleBy3 = table[i] + sumOfNumbersDivisibleBy3;
-            }
-        }
-        std::cout << "suma liczb podzielnych przez 3 = " << sumOfNumbersDivisibleBy3 << "\n";
-    }
+	void SumaPodziel3()
+	{
+		int sumOfNumbersDivisibleBy3 = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			if (table[i] % 3 == 0)
+			{
+				sumOfNumbersDivisibleBy3 = table[i] + sumOfNumbersDivisibleBy3;
+			}
+		}
+		std::cout << "suma liczb podzielnych przez 3 = " << sumOfNumbersDivisibleBy3 << "\n";
+	}
 
-    void showAllNumbers()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            std::cout << (i+1) << " liczba z tabeli = " << table[i]<<"\n";
-        }
-    }
+	void showAllNumbers()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			std::cout << (i + 1) << " liczba z tabeli = " << table[i] << "\n";
+		}
+	}
 
-    void zakresLicz(int lowIndex = 10, int highIndex = 60);
-    {
-        for (int i = 0; i < 10; i++)
-        {
+	void zakresLiczb(int lowIndex, int highIndex)
+	{
+		if (lowIndex >= 0 && highIndex <= 10)
+		{
+			for (int i = lowIndex; i < highIndex; i++)
+			{
+				std::cout << (i + 1) << " low i high index liczba z tabeli = " << table[i] << "\n";
+			}
+		}
+		else {
+			std::cout << "Podana liczba jest nie oprawid³owa \n";
+		}
 
-        }
-
-    }
+	}
 
 };
 
 
 int main()
 {
-    srand(time(NULL));
-    Sumator a(10); 
-    a.sumOfNumbers();
-    a.SumaPodziel3();
-    a.showAllNumbers();
+	srand(time(NULL));
+	Sumator a(10);
+	a.sumOfNumbers();
+	a.SumaPodziel3();
+	a.showAllNumbers();
+	a.zakresLiczb(2, 6);
 
 }
